9484c50a4f8ea9c9a26563d9eb769009c7183676
wangyu0627 <107354995+wangyu0627@users.noreply.github.com>
2025-08-01 16:41:24 +0800

    Update mllm_sft.sh

--- train/mllm_sft.sh
+++ train/mllm_sft.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 source /home/share/anaconda3/bin/activate llama_factory
-export PYTHONPATH="/data/wy/MLLMRec/LLaMA-Factory:$PYTHONPATH"
+export PYTHONPATH="/data/my_name/MLLMRec/LLaMA-Factory:$PYTHONPATH"
 
 llamafactory-cli train \
     --stage sft \
     --do_train True \
-    --model_name_or_path /data/wy/MLLMRec/Qwen-2.5-VL-3B-Instruct \
+    --model_name_or_path /data/my_name/MLLMRec/Qwen-2.5-VL-3B-Instruct \
     --preprocessing_num_workers 16 \
     --finetuning_type lora \
     --template qwen2_vl \
     --flash_attn auto \
-    --dataset_dir /data/wy/MLLMRec/LLaMA-Factory/data \
+    --dataset_dir /data/my_name/MLLMRec/LLaMA-Factory/data \
     --dataset microlens_vl_train_sft_1 \
     --cutoff_len 4096 \
     --learning_rate 1e-04 \
@@ -25,7 +25,7 @@ llamafactory-cli train \
     --warmup_steps 0 \
     --packing False \
     --report_to none \
-    --output_dir /data/wy/MLLMRec/checkpoints/microlens/sft_1_lora_qwenvl \
+    --output_dir /data/my_name/MLLMRec/checkpoints/microlens/sft_1_lora_qwenvl \
     --bf16 True \
     --plot_loss True \
     --trust_remote_code True \
